edad967689a9050be9d48034be065d270412307e
lsh5665256 <490830076@qq.com>
2019-09-10 11:52:01 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        woshi AAP
+      </div>
+    );
+  }
+}
+
+export default App;
